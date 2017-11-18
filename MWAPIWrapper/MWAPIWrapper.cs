4189f3ddcfdc74881e1af65b0596db4fbef865a6
Clive Galway <evilc@evilc.com>
2017-11-18 22:59:27 +0000

    Hook WM_PAINT etc

--- MWAPIWrapper/MWAPIWrapper.cs
+++ MWAPIWrapper/MWAPIWrapper.cs
@@ -1,7 +1,9 @@
-﻿using ManagedWinapi.Windows;
+﻿using ManagedWinapi.Hooks;
+using ManagedWinapi.Windows;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,12 +14,17 @@ namespace MWAPIWrapper
     {
         private SystemListView lv;
         private SystemWindow lvWindow;
+        private Hook MyHook;
 
         public void Add(string _hwnd)
         {
             IntPtr hwnd = (IntPtr)Convert.ToInt32(_hwnd, 16);
             lvWindow = new SystemWindow(hwnd);
             lv = SystemListView.FromSystemWindow(lvWindow);
+
+            MyHook = new Hook(HookType.WH_CALLWNDPROC, false, false);
+            MyHook.Callback += MyHookCallback;
+            MyHook.StartHook();
         }
 
         public void SetRowColor(int row, uint color)
@@ -26,6 +33,35 @@ namespace MWAPIWrapper
             rect.Top = lv[row].Position.Y;
             rect.Bottom = lv[row + 1].Position.Y;
             lvWindow.FillRect(rect, color);
+        }
+
+        private static int MyHookCallback(int code, IntPtr wParam, IntPtr lParam, ref bool callNext)
+        {
+            if (code >= 0)
+            {
+                // You will need to define the struct
+                var message = (CWPSTRUCT)Marshal.PtrToStructure(lParam, typeof(CWPSTRUCT));
+                switch (message.message)
+                {
+                    case 0x0014: // WM_ERASEBKGND
+                        var a = 1;
+                        break;
+                    case 0x000F: // WM_PAINT
+                        var b = 1;
+                        break;
+                }
+                // Do something with the data
+            }
+            return 0; // Return value is ignored unless you set callNext to false
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct CWPSTRUCT
+        {
+            public IntPtr lparam;
+            public IntPtr wparam;
+            public int message;
+            public IntPtr hwnd;
         }
     }
 
